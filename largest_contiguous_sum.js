645fec2742964bd8a3258fddb9541563a7c586f8
patricksandquist <patricksandquist@gmail.com>
2015-12-03 15:47:50 -0500

    Optimize solution

--- largest_contiguous_sum.js
+++ largest_contiguous_sum.js
@@ -1,10 +1,21 @@
 // Given an array of integers (positive and negative) find the largest continuous sum.
 function lcs(array) {
-  var current_sum = 0;
+  var current_sum = 0, max = 0;
   if (array.length !== 0) {
-    current_sum = array[0];
+    max = array[0];
   }
-  
+
+  for (var i = 0; i < array.length; i++) {
+    current_sum += array[i];
+    if (current_sum > max) {
+      max = current_sum;
+    }
+    if (current_sum < 0) {
+      current_sum = 0;
+    }
+  }
+
+  return max;
 }
 
 console.log(lcs([5, 3, -7, 6]) === 8);
