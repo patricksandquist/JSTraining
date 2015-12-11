b31633f2297672f59702924ade983584137dc912
patricksandquist <patricksandquist@gmail.com>
2015-12-11 12:19:22 -0800

    Add reverser problem

---
+++ wordReverser.js
@@ -0,0 +1,8 @@
+// Given an input string, reverse all the words.
+var reverser = function (str) {
+
+};
+
+console.log(reverser("Hello world!") === "world! Hello");
+console.log(reverser("My name is Patrick!") === "Patrick! is name My");
+console.log(reverser("") === "");
