6d9c808000246fd4b64601b0ef43a2c7cced53cc
patricksandquist <patricksandquist@gmail.com>
2015-12-11 12:29:46 -0800

    Add reverser solution

--- wordReverser.js
+++ wordReverser.js
@@ -1,8 +1,19 @@
 // Given an input string, reverse all the words.
 var reverser = function (str) {
+  var words = str.split(' ');
+  var cleanWords = [];
 
+  // If there are multiple spaces in a row, remove the extra empty words
+  words.forEach(function (word) {
+    if (word.length > 0) {
+      cleanWords.push(word);
+    }
+  });
+
+  return cleanWords.reverse().join(' ');
 };
 
 console.log(reverser("Hello world!") === "world! Hello");
+console.log(reverser("Hello    world!") === "world! Hello");
 console.log(reverser("My name is Patrick!") === "Patrick! is name My");
 console.log(reverser("") === "");
