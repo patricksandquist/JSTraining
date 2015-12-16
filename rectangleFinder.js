e9cedf013417b40fa5d08ad18a09f896752ca3f1
patricksandquist <patricksandquist@gmail.com>
2015-12-16 13:44:29 -0800

    Add rectangle problem

---
+++ rectangleFinder.js
@@ -0,0 +1,19 @@
+// Given a 2 dimensional array full of zeros, find the coordinates of the
+// rectangle of ones in it. Coordinates may be returned as [upper left, lower right]
+// [[0, 0, 0, 0, 0, 0],
+//  [0, 1, 1, 1, 0, 0],
+//  [0, 1, 1, 1, 0, 0],
+//  [0, 0, 0, 0, 0, 0]]
+// => [[1, 1], [2, 3]]
+
+
+// Extending this problem further, what if there were multiple rectangles of ones
+// in the array? Return a list of all of the rectangles' coordinates. You may assume
+// that the rectangles are always separated by zeros.
+// [[0, 0, 0, 0, 0, 0],
+//  [0, 1, 1, 1, 0, 0],
+//  [0, 1, 1, 1, 0, 0],
+//  [0, 0, 0, 0, 0, 0],
+//  [0, 0, 1, 1, 1, 1],
+//  [0, 0, 1, 1, 1, 1]]
+// => [[[1, 1], [2, 3]], [[4, 2], [5, 5]]]
