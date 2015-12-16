385cbb6158a383e8fc98665cb77de5135098fc23
patricksandquist <patricksandquist@gmail.com>
2015-12-16 13:51:28 -0800

    Add outline

--- rectangleFinder.js
+++ rectangleFinder.js
@@ -6,6 +6,15 @@
 //  [0, 0, 0, 0, 0, 0]]
 // => [[1, 1], [2, 3]]
 
+var rectangles_finder = function (arr) {
+  arr.forEach(function (row, i) {
+    
+  });
+};
+
+var bottom_right = function (arr, ul) {
+  // Given the upper left coordinates of a rectangle, return the bottom right
+};
 
 // Extending this problem further, what if there were multiple rectangles of ones
 // in the array? Return a list of all of the rectangles' coordinates. You may assume
