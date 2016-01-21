4d936b36bd92f73004c34e394461bc245ee9d065
patricksandquist <patricksandquist@gmail.com>
2016-01-20 16:29:26 -0800

    Add name basic

--- higherOrderExample.js
+++ higherOrderExample.js
@@ -14,10 +14,10 @@ function compareNumbers(x, y) {
 // In this example, the function compareNumbers is given as an
 // argument to the sort function. When sort is invoked on line
 // 17, it will in turn invoke compareNumbers to sort the array.
-[3, 1, 4, 1, 5, 9].sort(compareNumbers);
+console.log([3, 1, 4, 1, 5, 9].sort(compareNumbers));
 
 // This is the same as invoking the sort method with an anonymous function:
-[3, 1, 4, 1, 5, 9].sort(function(x, y) {
+console.log([3, 1, 4, 1, 5, 9].sort(function(x, y) {
   if (x < y) {
     return -1;
   } else if (x > y) {
@@ -25,4 +25,14 @@ function compareNumbers(x, y) {
   } else {
     return 0;
   }
-});
+}));
+
+// Higher-order functions can save a lot of time by eliminating boilerplate
+// code. Consider the following example where an array of names is made
+// upperscript.
+var names = ["Fred", "Wilma", "Pebbles"];
+var upper = [];
+for (var i = 0; i < names.length; i++) {
+  upper[i] = names[i].toUpperCase();
+}
+console.log(upper);
